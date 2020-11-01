42674c1f9cd59a7c897da2591cc4724a28c9f5bb
Elijam <elijam1608@gmail.com>
2020-11-01 18:44:50 +1100

    fixed some err

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,8 +6,6 @@ module.exports = app => {
   // HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
